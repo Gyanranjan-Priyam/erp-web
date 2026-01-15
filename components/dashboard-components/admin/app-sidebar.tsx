cef5bd60f5651f689983848ec28a3f50a7620d3d
Gyanranjan Priyam <contact.gyanranjan@gmail.com>
2026-01-15 17:17:43 +0530

    Project Initialize

---
+++ components/dashboard-components/admin/app-sidebar.tsx
@@ -0,0 +1,139 @@
+"use client"
+
+import * as React from "react"
+import {
+  IconCamera,
+  IconChartBar,
+  IconDashboard,
+  IconDatabase,
+  IconFileAi,
+  IconFileDescription,
+  IconFileWord,
+  IconFolder,
+  IconHelp,
+  IconInnerShadowTop,
+  IconListDetails,
+  IconReport,
+  IconSearch,
+  IconSettings,
+  IconUsers,
+} from "@tabler/icons-react"
+
+import { NavDocuments } from "@/components/dashboard-components/admin/nav-documents"
+import { NavMain } from "@/components/dashboard-components/admin/nav-main"
+import { NavSecondary } from "@/components/dashboard-components/admin/nav-secondary"
+import { NavUser } from "@/components/dashboard-components/admin/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: IconDashboard,
+    },
+    {
+      title: "Students",
+      url: "#",
+      icon: IconListDetails,
+    },
+    {
+      title: "Faculties",
+      url: "#",
+      icon: IconChartBar,
+    },
+    {
+      title: "Departments",
+      url: "#",
+      icon: IconFolder,
+    },
+    {
+      title: "Class Schedules",
+      url: "#",
+      icon: IconFolder,
+    },
+    {
+      title: "Payment",
+      url: "#",
+      icon: IconUsers,
+    },
+    {
+      title: "Admissions",
+      url: "#",
+      icon: IconUsers,
+    },
+  ],
+
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: IconSettings,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: IconSearch,
+    },
+  ],
+  documents: [
+    {
+      name: "Students Reports",
+      url: "#",
+      icon: IconDatabase,
+    },
+    {
+      name: "College Payments Reports",
+      url: "#",
+      icon: IconReport,
+    },
+    {
+      name: "Hostel Payments Reports",
+      url: "#",
+      icon: IconFileWord,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">Acme Inc.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavDocuments items={data.documents} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
